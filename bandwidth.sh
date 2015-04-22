5438e880d88560a9d4ac1d13859fb167b92416ce
completenovice <i7212169@bournemouth.ac.uk>
2015-04-22 04:14:31 -0700

    bash script for running bandwidth checker and outputting values in csv

---
+++ bandwidth.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+receiveFile="/sys/class/net/eth0/statistics/rx_bytes"
+transmitFile="/sys/class/net/eth0/statistics/tx_bytes"
+
+r1=$(cat "$receiveFile")
+t1=$(cat "$transmitFile")
+sleep 0.1
+r2=$(cat "$receiveFile")
+t2=$(cat "$transmitFile")
+
+#total = reading2 - reading1, convert to kilobytes, then to kilobits, then making it a 1sec reading (not 100ms)
+let rf="(($r2 - $r1) / 1024) * 8 * 10"
+let tf="(($t2 - $t1) / 1024) * 8 * 10"
+
+#print to csv file (kilobits per second >> download, upload)
+echo $rf,$tf > bandwidth.csv
