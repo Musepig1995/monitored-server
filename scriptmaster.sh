5ad0c5bca3372fd67265ef6f2caa811a8bac9f18
completenovice <i7212169@bournemouth.ac.uk>
2015-04-24 08:09:40 -0700

    ammended sendserverusage script (and affiliated script(s))

--- scriptmaster.sh
+++ scriptmaster.sh
@@ -6,13 +6,14 @@ i="0"
 while [ $i -lt 30 ]
 do
 
-/var/local/get_bandwidth.sh
-/var/local/get_memory_usage.sh
-/var/local/get_cpu_usage.sh
-/var/local/get_ip.sh
-php /var/local/total_diskspace.txt
-php /var/local/arrayGenerator.txt
+	/var/local/get_bandwidth.sh
+	/var/local/get_memory_usage.sh
+	/var/local/get_cpu_usage.sh
+	/var/local/get_ip.sh
+	php /var/local/total_diskspace.txt
+	php /var/local/arrayGenerator.txt
+	php /var/local/send_server_usage.txt
 
-sleep 1.85 
-i=$[$i+1]
+	sleep 1.85 
+	i=$[$i+1]
 done
